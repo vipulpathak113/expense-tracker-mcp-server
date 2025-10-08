7473668fe79cba8086db088fa84690440bc924ea
Yoda_Vipul <vipulpathak113@gmail.com>
2025-10-09 00:50:56 +0530

    initialize db

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-def main():
-    print("Hello from expense-tracker-mcp-server!!")
+from fastmcp import FastMCP
+import os
+import sqlite3
+
+DB_PATH = os.path.join(os.path.dirname(__file__), 'expenses.db')
+
+def initialize_db():
+    with sqlite3.connect(DB_PATH) as conn:
+        conn.execute("""
+            CREATE TABLE IF NOT EXISTS expenses (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                amount REAL NOT NULL,
+                category TEXT NOT NULL,
+                date TEXT NOT NULL,
+                subcategory TEXT DEFAULT '',
+                note TEXT DEFAULT ''
+            )
+        """)
+initialize_db()        
+
+mcp= FastMCP('Expnse Tracker')
 
 
 if __name__ == "__main__":
-    main()
+    mcp.run()
